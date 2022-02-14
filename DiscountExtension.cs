b44494fcef1f3493dc40dd065436121f7a61c302
Сильвестров Иван <sealwestrow@gmail.com>
2022-02-14 11:51:42 +0300

    Добавьте файлы проекта.

---
+++ DiscountExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopLibrary;
+
+namespace NewDiscount
+{
+    public static class DiscountExtension
+    {
+        public static double GetTotalWithOtherDiscount(this Order NewOrder)
+        {
+            int fp = NewOrder.Customer.Total;            
+            if (fp > 50000)
+                return 15;
+            else return 0;
+        }
+    }
+}
